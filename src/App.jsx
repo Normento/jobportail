3f9cfe2a9ca7961bd7c07442891ee96a88978de5
Norman <normento229@gmail.com>
2023-03-02 18:29:23 +0100

    creating component and add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Footer from "./components/FooterDiv/Footer";
+import Jobs from "./components/JobDiv/Jobs";
+import Navbar from "./components/NavBar/Navbar";
+import Search from "./components/SearchDiv/Search";
+import Value from "./components/ValueDiv/Value";
 
 const App = () => {
-  return <div>Hello world app</div>;
+  return (
+    <div className=" w -[85%] m-auto bg-white">
+      <Navbar />
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer/>
+    </div>
+  )
 };
 
 export default App;
